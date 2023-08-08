f4c83472b62899522b96b35cbc3fbefcb8488839
22012871-Triston <22012871@myrp.edu.sg>
2023-08-08 08:37:49 +0800

    My part - Triston

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,10 +5,10 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 
 		ArrayList<UserAcct> UserList = new ArrayList<UserAcct>();
-		
 
-		UserList.add(new UserAcct("Alice", 87551131, ""));
-		UserList.add(new UserAcct("Bob", 97891432, ""));
+
+		UserList.add(new UserAcct("Alice", 87551131, "alice123"));
+		UserList.add(new UserAcct("Bob", 97891432, "bob456"));
 
 		int option = -1;
 
@@ -21,14 +21,16 @@ public class C206_CaseStudy {
 				C206_CaseStudy.EventMenu();
 
 			} else if (option == 2) {
-				C206_CaseStudy.addUserAcct(UserList);
+				UserAcct UA = inputUserAcct();
+				C206_CaseStudy.addUserAcct(UserList, UA);
 
 			} else if (option == 3) {
 				C206_CaseStudy.viewAllUserList(UserList);
-				
+
 			}else if(option == 4){
-				C206_CaseStudy.deleteUserAcct(UserList);
-				
+				String name = Helper.readString("Enter Name > "); 
+				C206_CaseStudy.deleteUserAcct(UserList,name);
+
 			}else if(option == 5){
 				System.out.println("Bye!");
 			}else {
@@ -62,51 +64,80 @@ public class C206_CaseStudy {
 		Helper.line(115, "-");
 	}
 	//================================= Option 1 add (CRUD - Read) =================================
-	
-	public static void addUserAcct(ArrayList<UserAcct> UserList) {
-	
+
+	public static UserAcct inputUserAcct() {
+
 		String name = Helper.readString("Enter Name > ");
 		int contactNum = Helper.readInt("Enter Contact Number > ");
 		String userID  = Helper.readString("Enter UserID > ");
-		
-		UserList.add(new UserAcct(name,contactNum,userID));
+
+		UserAcct UA = new UserAcct(name, contactNum, userID);
+		return UA;
 	}
+
+	public static void addUserAcct(ArrayList<UserAcct> UserList, UserAcct UA) {
+
+		for(int i = 0; i < UserList.size(); i++) {
+
+			if (UserList.get(i).getName().equalsIgnoreCase(UA.getName()) )
+				return;
+		}
+		if ((UA.getName().isEmpty()) || UA.getContactnum() == 0) {
+			return;
+		}
+		UserList.add(UA) ;
+	}
+
+
+
+
 
 	//================================= Option 2 view (CRUD - Create)=================================
+
 	public static String retrieveAllUserAcct(ArrayList<UserAcct> UserList) {
 		String output = "";
-
 		for (int i = 0; i < UserList.size(); i++) {
-				output += String.format("%-10s %-15d %-10s\n", UserList.get(i).getName(), UserList.get(i).getContactnum(),
-				UserList.get(i).getUserID());
-			}
+			output += String.format("%-10s %-15d %-10s\n", UserList.get(i).getName(), UserList.get(i).getContactnum(),
+					UserList.get(i).getUserID());
+		}
 		return output;
 	}
-	
+
+
 	public static void viewAllUserList(ArrayList<UserAcct> UserList) {
 		C206_CaseStudy.setHeader("CAMCORDER LIST");
 		String output = String.format("%-10s %-15s %-10s\n", "Name", "ContactNum", "UserID");
-		 output += retrieveAllUserAcct(UserList);	
+		output += retrieveAllUserAcct(UserList);	
 		System.out.println(output);
-	
+
 	}
 	//================================= Option 3 delete (CRUD - Create)=================================
-	public static void deleteUserAcct(ArrayList<UserAcct> UserList) {
-	String name = Helper.readString("Enter Name > ");
-	for (int i = 0; i < UserList.size(); i++) {
-	if (name.equalsIgnoreCase(UserList.get(i).getName())) {
-		char option = Helper.readChar("Are you sure you want to delete (Y/N) > ");
-		if (option == 'y' || option == 'Y') {
-			UserList.remove(UserList.get(i));
-		}else if(option == 'n' || option == 'N') {
-				System.out.println("User will not be deleted");
-		}else {
-			System.out.println("Invalid option");
+	public static String checkdeleteUserAcct(ArrayList<UserAcct> UserList) {    
+		String output = "";
+		for (int i = 0; i < UserList.size(); i++) {
+			output += String.format("%-10s %-15d %-10s\n", UserList.get(i).getName(), UserList.get(i).getContactnum(),
+					UserList.get(i).getUserID());
+		}
+		return output;
+	}
+
+	public static boolean deleteUserAcct(ArrayList <UserAcct> UserList, String name) {
+		//String output = checkdeleteUserAcct(UserList);
+		String checkname = name.trim();
+		for (int i = 0; i < UserList.size(); i++) {
+			if (checkname.equalsIgnoreCase(UserList.get(i).getName())) {      
+					
+				UserList.remove(UserList.get(i));
+					return(true);
+			}else {
+				return(false);
 			}
 		}
-				
-			}
+		return(false);
 	}
 }
+
+
+
 
 
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,102 @@
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	// prepare test data
+	private UserAcct UA1;
+	private UserAcct UA2;
+
+	private ArrayList<UserAcct> UserList;
 
 	@Before
 	public void setUp() throws Exception {
+		UA1 = new UserAcct("Alice", 87551131, "");
+		UA2 = new UserAcct("Bob", 97891432, "");
+
+		UserList = new ArrayList<UserAcct>();
+	}
+
+	@Test
+	public void testAddUserAcct() {
+		// Create a new UserAcct object
+		UserAcct user1 = new UserAcct("John Doe", 12345678, "johndoe123");
+
+		// Ensure that the UserList is empty before adding the user
+		assertEquals(0, UserList.size());
+
+		// Add the user to the UserList using the addUserAcct method
+		C206_CaseStudy.addUserAcct(UserList, user1);
+
+		// Check if the user was added successfully
+		assertEquals(1, UserList.size());
+		assertEquals("John Doe", UserList.get(0).getName());
+		assertEquals(12345678, UserList.get(0).getContactnum());
+		assertEquals("johndoe123", UserList.get(0).getUserID());
+
+		// Try adding the same user again and ensure it is not duplicated
+		C206_CaseStudy.addUserAcct(UserList, user1);
+		assertEquals(1, UserList.size());
+	}
+
+	@Test
+	public void testAddUserAcct_InvalidInput() {
+		// Create a new UserAcct object with empty fields
+		UserAcct user2 = new UserAcct("", 0, "");
+
+		// Ensure that the UserList is empty before adding the user
+		assertEquals(0, UserList.size());
+
+		// Try adding the user with empty fields and ensure it is not added
+		C206_CaseStudy.addUserAcct(UserList, user2);
+		assertEquals(0, UserList.size());
+	}
+
+	@Test
+	public void testRetrieveAllAcct() {
+		assertNotNull("Test if there is a valid UserAcct arraylist to retrieve item", UserList);
+
+		// Test if the list of UserAcct retrieved from the UserList is empty (boundary case)
+		String allUserAcct = C206_CaseStudy.retrieveAllUserAcct(UserList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllUserAcctList", testOutput, allUserAcct);
+
+		// Given an empty list, after adding 2 UserAcct objects, test if the size of the list is 2 (normal case)
+		C206_CaseStudy.addUserAcct(UserList, UA1);
+		C206_CaseStudy.addUserAcct(UserList, UA2);
+		assertEquals("Test that UserAcct arraylist size is 2", 2, UserList.size());
+
+		// Test if the expected output string matches the list of UserAcct retrieved from the UserList
+		allUserAcct = C206_CaseStudy.retrieveAllUserAcct(UserList);
+		testOutput = String.format("%-10s %-15d %-10s\n", "Alice", 87551131, "");
+		testOutput += String.format("%-10s %-15d %-10s\n", "Bob", 97891432, "");
+
+		assertEquals("Test that viewAllUserAcct", testOutput, allUserAcct);
+	}
+
+	@Test
+	public void testdeleteUSerAcct() {
+		UserList.add(UA2);
+		assertFalse("Test that user is not found in the arrayList",
+				C206_CaseStudy.deleteUserAcct(UserList, "mike" ));
+		assertTrue("Test that user is in the arrayList",C206_CaseStudy.deleteUserAcct(UserList, "Bob" ));
+		
+		UserList.add(UA1);
+		assertTrue("Test that user is in the arrayList",C206_CaseStudy.deleteUserAcct(UserList, "Alice" ));
 	}
 
 	@After
 	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+		UA1 = null;
+		UA2 = null;
+		UserList = null;
 	}
 
 }
